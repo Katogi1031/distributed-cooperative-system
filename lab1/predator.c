3e698c344893d1f67069c81294ec105751d1160c
Katogi1031 <103903558+Katogi1031@users.noreply.github.com>
2022-10-25 18:03:38 +0900

    test
    一応A*は実行できたので整形した
    次からは実行できるように変更する

--- lab1/predator.c
+++ lab1/predator.c
@@ -47,10 +47,6 @@ void ReconstructThePath(struct node* goalNode);
 struct node* AStarAlgorithm(struct node* current, struct node* goal, struct node **openList, int l1, struct node** closedList, int l2){
   int i, j, nextIndex;
 
-  static int count = 0;
-  printf("count = %d\n", count);
-  count++;
-  
   /* オープンリストの長さを求める */
   l1 = ExpandNode(current, openList, l1, closedList, l2);
 
@@ -63,17 +59,6 @@ struct node* AStarAlgorithm(struct node* current, struct node* goal, struct node
   
   /* 次のオープンリストを作成する */
   struct node *tempList = (struct node*)calloc(l1-1, sizeof(struct node));
-  
-  // for(i = 0, j = 0; i < l1; i++){
-  //   if(i != nextIndex){
-  //     memcpy(&tempList[j], &((*openList)[i]), sizeof(struct node));
-  //     j++;
-  //   }
-  // }
-  // l1--;
-  // l2++;
-  // *openList = tempList;
-
   for(i = 0,j=0;i<l1;i++){
     if(i != nextIndex){
       memcpy(&tempList[j],&((*openList)[i]),sizeof(struct node));
@@ -86,8 +71,6 @@ struct node* AStarAlgorithm(struct node* current, struct node* goal, struct node
   
 
   /* クローズドノードに追加していく */
-  // *closedList = (struct node*)realloc((*closedList), l2*(sizeof(struct node)));
-  // memcpy(&(*closedList)[l2-1], nextNode, sizeof(struct node));
   *closedList = (struct node*)realloc((*closedList), l2*(sizeof(struct node)));
   memcpy(&((*closedList)[l2-1]),nextNode,sizeof(struct node));
 
@@ -105,7 +88,6 @@ int ExpandNode(struct node* current, struct node **openList, int l1, struct node
   int i, j, count, found;
   count = 0;
 
-  // struct node* tempList = (struct node*)calloc(4, sizeof(struct node));
   struct node* tempList;
    tempList = (struct node*)calloc(4,sizeof(struct node));   
    for(i = 0;i<4;i++){
@@ -121,30 +103,6 @@ int ExpandNode(struct node* current, struct node **openList, int l1, struct node
     
       tempList[i].parent = current;
    }
-  // for(i = 0; i < 4; i++){
-  //   tempList[i].pnt = predatorCreatePosition();
-    
-  //   if(i == 0 && gfield[current->pnt->x-1][current->pnt->y] != -1){
-  //     tempList[i].pnt->x = current->pnt->x-1;
-  //     tempList[i].pnt->y = current->pnt->y;
-  //     tempList[i].g = current->g + 1.0f;
-  //   }else if(i == 1 && gfield[current->pnt->x+1][current->pnt->y] != -1){
-  //     tempList[i].pnt->x = current->pnt->x+1;
-  //     tempList[i].pnt->y = current->pnt->y;
-  //     tempList[i].g = current->g + 1.0f;
-  //   }else if(i == 2 && gfield[current->pnt->x][current->pnt->y-1] != -1){
-  //     tempList[i].pnt->x = current->pnt->x;
-  //     tempList[i].pnt->y = current->pnt->y-1;
-  //     tempList[i].g = current->g + 1.0f;
-  //   }else if(i == 3 && gfield[current->pnt->x][current->pnt->y+1] != -1){
-  //     tempList[i].pnt->x = current->pnt->x;
-  //     tempList[i].pnt->y = current->pnt->y+1;
-  //     tempList[i].g = current->g + 1.0f;
-  //   }
-
-  //   tempList[i].parent = current;
-      
-  // }
 
   /* 現在のノードが小ノードを持つかどうか*/
   for(j = 0; j < 4; j++){
@@ -163,27 +121,6 @@ int ExpandNode(struct node* current, struct node **openList, int l1, struct node
         memcpy(&((*openList)[total-1]),&tempList[j],sizeof(struct node));              
       }      
    } 
-  // for(j = 0; j < 4; j++){
-  //   found = 0;
-  //   if(tempList[j].parent == NULL){
-  //     return 0;
-  //   }
-  //   for(i = 0;i<l1;i++){
-  //     if((*openList)[i].pnt->x == tempList[j].pnt->x && (*openList)[i].pnt->y == tempList[j].pnt->y)
-  //       found++;
-  //   } 
-  //   for(i = 0;i<l2;i++){
-  //     if(tempList[j].pnt->x == (*closedList)[i].pnt->x && (*closedList)[i].pnt->y == tempList[j].pnt->y)
-  //       found++; 
-    
-  //   }
-  //   if(found == 0){
-  //     count++;
-  //     int total = (1l + count);
-  //     *openList = (struct node*)realloc((*openList), total*(sizeof(struct node)));
-  //     memcpy(&((*openList)[total-1]), &tempList[j], sizeof(struct node)); 
-  //   }
-  // }
   return count + l1;
 }
 
@@ -191,15 +128,6 @@ int ExpandNode(struct node* current, struct node **openList, int l1, struct node
 
 /* CalculateTheTotalCost */
 void CalculateTheTotalCost(struct node* goalNode, struct node **openList, int l1){
-  // int i, difx, dify;
-  // int x = (*openList)[0].pnt->x;
-  printf("test\n");
-  // for(i = 0;i<l1;i++)
-  //   {
-  //       difx = (*openList)[i].pnt->x - goalNode->pnt->x;
-  //       dify = (*openList)[i].pnt->y - goalNode->pnt->y;
-  //       (*openList)[i].h = ((float)sqrt(pow(difx,2) + pow(dify,2)));
-  //   }
   int i,difx,dify;
   for(i = 0; i < l1; i++){
     difx = (*openList)[i].pnt->x - goalNode->pnt->x;
@@ -210,17 +138,6 @@ void CalculateTheTotalCost(struct node* goalNode, struct node **openList, int l1
 
 /* FindTheLeastCosted */
 int FindTheLeastCosted(struct node **openList, int l1){
-  // int i, min, minIndex;
-  // min = (*openList)[0].g + (*openList)[0].h;
-  // minIndex = 0;
-  // for(i = 1; i < l1; i++){
-  //   if((*openList)[i].g * (*openList)[i].h < min){
-  //     min = (*openList)[i].g + (*openList)[i].h;
-  //     minIndex = i;
-  //   }
-  // }
-  // return minIndex;
-
   int i, min, minIndex;
     min = (*openList)[0].g + (*openList)[0].h;
     minIndex = 0;
@@ -235,10 +152,6 @@ int FindTheLeastCosted(struct node **openList, int l1){
 
 /* ReconstructThePath */
 void ReconstructThePath(struct node* goalNode){
-  // struct node* current = goalNode;
-  // struct point* ptr = NULL;
-  // int steps = 0, i;
-  
   struct node* current = goalNode;
   struct point* ptr = NULL;
   int steps = 0,i;
@@ -249,7 +162,6 @@ void ReconstructThePath(struct node* goalNode){
       current = current->parent;                    
   }
   for(i = steps; i >= 1; i--){
-    // field[ptr[i-1].x][ptr[i-1].y] = 20;
     printf("(%d,%d)",ptr[i-1].x,ptr[i-1].y);
     if(i>1)  printf("=>");  
   }
@@ -269,8 +181,6 @@ void predator(int *ca){
   struct point *predator, *prey;
   int openLen = 0, closedLen = 0; // オープンリスト、クローズリストの長さ
 
-  
-
   predator = predatorCreatePosition();
   prey = predatorCreatePosition();
 
@@ -288,10 +198,7 @@ void predator(int *ca){
         prey->x = p, prey->y = q;
         break;
     }
-
   }
-
- 
 
   printf("predator (%d %d)\n", predator->x, predator->y);
   printf("prey     (%d %d)\n", prey->x, prey->y);
