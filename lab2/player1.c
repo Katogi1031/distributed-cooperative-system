79f6d4de242846e98a086f8645c22eb68491bb12
Katogi1031 <103903558+Katogi1031@users.noreply.github.com>
2022-12-12 12:53:16 +0900

    lab2

--- lab2/player1.c
+++ lab2/player1.c
@@ -1,75 +1,36 @@
 #include<stdio.h>
 
-// 平衡点推定法
-int estimate(int player1, int player2) {
-	// プレイヤー1が実際に選択した戦略
-	int strategy1 = player1;
-	
-	// プレイヤー2が実際に選択した戦略
-	int strategy2 = player2;
-	
-	// 平衡点を推定する
-	int equilibrium = 0;
-	if (strategy1 == 1 && strategy2 == 1) {
-		// 両者が協調した場合の平衡点
-		equilibrium = 3;
-	} else if (strategy1 == 1 && strategy2 == 0) {
-		// プレイヤー1が協調し、プレイヤー2が裏切った場合の平衡点
-		equilibrium = 0;
-	} else if (strategy1 == 0 && strategy2 == 1) {
-		// プレイヤー1が裏切り、プレイヤー2が協調した場合の平衡点
-		equilibrium = 5;
-	} else if (strategy1 == 0 && strategy2 == 0) {
-		// 両者が裏切った場合の平衡点
-		equilibrium = 1;
+int player1_scores(int player1Strategy, int player2Strategy) {
+	int score = 0;
+	if (player1Strategy == 1 && player2Strategy == 1) {			// 両者が協調した場合
+		score = 3;
+	} else if (player1Strategy == 1 && player2Strategy == 0) {	// プレイヤー1が協調し、プレイヤー2が裏切った場合
+		score = 0;
+	} else if (player1Strategy == 0 && player2Strategy == 1) {	// プレイヤー1が裏切り、プレイヤー2が協調した場合
+		score = 5;
+	} else if (player1Strategy == 0 && player2Strategy == 0) {	// 両者が裏切った場合の平衡点
+		score = 1;
 	}
-	
-	return equilibrium;
+	return score;
 }
 
 // プレイヤーが選択する戦略を決定する
-int decision(int player1, int player2) {
-	// 平衡点を推定する
-	int equilibrium = estimate(player1, player2);
-	
-	// プレイヤーが選択する戦略
+int player1_get_action(int player1Strategy, int player2Strategy) {
+	int score = player1_scores(player1Strategy, player2Strategy);
 	int strategy = 0;
-	if (equilibrium > 3) {
-		// 平衡点が3より大きい場合は協調する
+	if (score > 3) {	// 3より大きい場合は協調する
 		strategy = 1;
-	} else {
-		// 平衡点が3以下の場合は裏切る
+	} else {			// 3以下の場合は裏切る
 		strategy = 0;
 	}
-	
 	return strategy;
 }
 
 Player1(int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer1Strategy){
-    // static int array1[10];
-    static int i = 0;
-	static int cnt = 0;
-    // array1[i] = i;
-    // i++;
-    // int num = sizeof(array1);
-    // // printf("num = %d\n", i);
-    // for(int j = 0; j < i; j++){
-    //     printf("array = %d\n", array1[j]);
-    // }
-    // *currentPlayer1Strategy = 1;
-	
-	// プレイヤーが選択する戦略を決定する
     if(previousPlayer1Strategy == -1){
         *currentPlayer1Strategy = 1;
         printf("player2 action 1\n");
     }else{
-        int num = decision(previousPlayer1Strategy, previousPlayer2Strategy);
-        printf("player1 action %d, cnt=%d\n", num, cnt);
-        *currentPlayer1Strategy = num;
+        *currentPlayer1Strategy = player1_get_action(previousPlayer1Strategy, previousPlayer2Strategy);
     }
-
-	if(*currentPlayer1Strategy == 1){
-		cnt++;
-	}
-
 }
--- lab2/player2.c
+++ lab2/player2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 // 2つの式の最大値を求める
-void maximize(int exp1, int exp2, int *x, int *y) {
+void player2_maximize(int exp1, int exp2, int *x, int *y) {
 	// フェルマーの未定乗数法を用いて最大値を求める
 	*x = 0;
 	*y = 0;
@@ -13,96 +13,33 @@ void maximize(int exp1, int exp2, int *x, int *y) {
 	}
 }
 
-// int solve(int player1, int player2) {
-// 	// プレイヤー1が実際に選択した戦略
-// 	int strategy1 = player1;
-	
-// 	// プレイヤー2が実際に選択した戦略
-// 	int strategy2 = player2;
-	
-// 	// 囚人のジレンマをフェルマーの未定乗数法で解く
-// 	int x, y;
-// 	maximize(3 * x + y, 3 * y + x, &x, &y);
-	
-// 	// プレイヤー1が得る利得
-// 	int gain1 = 0;
-// 	if (strategy1 == 1 && strategy2 == 1) {
-// 		// 両者が協調した場合のプレイヤー1が得る利得
-// 		gain1 = x;
-// 	} else if (strategy1 == 1 && strategy2 == 0) {
-// 		// プレイヤー1が協調し、プレイヤー2が裏切った場合のプレイヤー1が得る利得
-// 		gain1 = 0;
-// 	} else if (strategy1 == 0 && strategy2 == 1) {
-// 		// プレイヤー1が裏切り、プレイヤー2が協調した場合のプレイヤー1が得る利得
-// 		gain1 = 5;
-// 	} else if (strategy1 == 0 && strategy2 == 0) {
-// 		// 両者が裏切った場合のプレイヤー1が得る利得
-// 		gain1 = 1;
-// 	}
-// }
-
 // 囚人のジレンマゲームのプレイヤーのアクションを求める
-int get_action(int player1, int player2) {
-    // プレイヤー1が実際に選択した戦略
-    int strategy1 = player1;
-
-    // プレイヤー2が実際に選択した戦略
-    int strategy2 = player2;
-
+int player2_get_action(int player1Strategy, int player2Strategy) {
     // 囚人のジレンマをフェルマーの未定乗数法で解く
     int x, y;
-    maximize(3 * x + y, 3 * y + x, &x, &y);
-
+    player2_maximize(3 * x + y, 3 * y + x, &x, &y);
+    if(x >= 1) x = 1;
+    if(y >= 1) y = 1;
     // プレイヤー1が実際に取るべきアクション
-    int action1 = 0;
-    if (strategy1 == 1 && strategy2 == 1) {
-        // 両者が協調した場合のプレイヤー1が取るべきアクション
-        action1 = x;
-    } else if (strategy1 == 1 && strategy2 == 0) {
-        // プレイヤー1が協調し、プレイヤー2が裏切った場合のプレイヤー1が取るべきアクション
-        action1 = y;
-    } else if (strategy1 == 0 && strategy2 == 1) {
-        // プレイヤー1が裏切り、プレイヤー2が協調した場合のプレイヤー1が取るべきアクション
-        action1 = y;
-    } else if (strategy1 == 0 && strategy2 == 0) {
-        // 両者が裏切った場合のプレイヤー1が取るべきアクション
-        action1 = x;
+    int action2 = 0;
+    if (player1Strategy == 1 && player2Strategy == 1) {         // 両者が協調した場合
+        action2 = x;
+    } else if (player1Strategy == 1 && player2Strategy == 0) {  // プレイヤー1が協調し、プレイヤー2が裏切った場合
+        action2 = y;
+    } else if (player1Strategy == 0 && player2Strategy == 1) {  // プレイヤー1が裏切り、プレイヤー2が協調した場合
+        action2 = y;
+    } else if (player1Strategy == 0 && player2Strategy == 0) {  // 両者が裏切った場合
+        action2 = x;
     }
-
-    return action1;
+    return action2;
 }
 
 Player2(int previousPlayer1Strategy, int previousPlayer2Strategy, int *currentPlayer2Strategy){
-
-    // int cooperate = 1; // 協調
-    // int deception = 0; // 裏切り
-    // static int previousPlayer1StrategyArray[10];
-    // static int previousPlayer2StrategyArray[10];
-    // static int player2chosenStrategy; // 今回player2が選択した戦略
-    static int i = 0;
-    // // 最初は協調する
-    // if (i = 0 && previousPlayer1Strategy == -1 && previousPlayer2Strategy == -1){
-    //     *currentPlayer2Strategy = 1;
-    // }else{
-    //     // 前回の両プレイヤーの戦略を保存する
-    //     previousPlayer1StrategyArray[i] = previousPlayer1Strategy;
-    //     previousPlayer2StrategyArray[i] = previousPlayer2Strategy;
-    //     i++;
-
-
-    // }
-    // // エラー率による戦略反転が起きたかどうかを判定するために保存
-    // player2chosenStrategy = *currentPlayer2Strategy;
+    // 最初は協調する
     if(previousPlayer2Strategy == -1){
         *currentPlayer2Strategy = 1;
-        printf("player2 action 1\n");
     }else{
-        int num = get_action(previousPlayer1Strategy, previousPlayer2Strategy);
-        printf("player2 action %d\n", num);
-        *currentPlayer2Strategy = num;
+        *currentPlayer2Strategy = player2_get_action(previousPlayer1Strategy, previousPlayer2Strategy);
     }
-    
-    // *currentPlayer2Strategy
-
 }
 
