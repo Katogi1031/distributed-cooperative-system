55e5483a540b743c2707f7fc44a84fb18671863f
katogi1031 <103903558+Katogi1031@users.noreply.github.com>
2023-01-13 10:01:50 +0900

    add UnreachableMap Function
    4x4のグリッドに16x16のグリッドで未踏の地がいくつあるのかを計算

--- lab3/predator.c
+++ lab3/predator.c
@@ -26,19 +26,40 @@ void MapUpdate(int field[16][16]){
     }
 }
 
+// 未踏の地が4x4のグリッドにいくつ存在するか
+void UnreachableMap(int field[4][4]){
+    int i, j, p, q, cnt;
+    for(i = 0; i < 16; i++){
+        cnt = 0;
+        for(j = 0; j < 16; j++){
+            p = i / 4;
+            q = j / 4;
+            if(map[i][j] == -10) field[p][q]+=1;
+            
+            
+        }
+        
+    }
+}
+
 
 void Predator(int field1[16][16], int field2[16][16], int field3[16][16], int field4[16][16], int *point1, int *point2, int *point3, int *point4){
 
-    Predator1(field1, point1);
-    Predator2(field2, point2);
-    Predator3(field3, point3);
-    Predator4(field4, point4);
+    // 16x16のマップを4x4のマップで表現
+    // 要素の値は未踏(mapで-10)の個数を表現
+    int quarterMap[4][4] = {{0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}
+                           };
 
     // 各predatorの視界からマップを更新
     MapUpdate(field1);
     MapUpdate(field2);
     MapUpdate(field3);
     MapUpdate(field4);
+
+    UnreachableMap(quarterMap);
 
     // mapの出力
     for(int i = 0; i < 16; i++){
@@ -47,6 +68,20 @@ void Predator(int field1[16][16], int field2[16][16], int field3[16][16], int fi
         }
         printf("\n");
     }
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            printf("%3d", quarterMap[i][j]);
+        }
+        printf("\n");
+    }
+
+    // predatorの行動戦略を取得
+    Predator1(field1, point1);
+    // Predator2(field2, point2);
+    // Predator3(field3, point3);
+    // Predator4(field4, point4);
+
 }
 
 void Predator1(int* field, int* point){
@@ -54,14 +89,16 @@ void Predator1(int* field, int* point){
     // printf("%d\n", *point);
 }
 
-void Predator2(int* field, int* point){
-    *point = (int)("u");   
-}
+// void Predator2(int* field, int* point){
+//     *point = (int)("u");   
+// }
 
-void Predator3(int* field, int* point){
-    *point = (int)("u");
-}
+// void Predator3(int* field, int* point){
+//     *point = (int)("u");
+// }
 
-void Predator4(int* field, int* point){
-    *point = (int)('d');    
-}
+// void Predator4(int* field, int* point){
+//     *point = (int)('d');    
+// }
+    
+
